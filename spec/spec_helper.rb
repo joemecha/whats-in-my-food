9234cc1991f4bde9e868ba47137dcb7d48482673
joemecha <jwmecha@gmail.com>
2021-06-02 09:01:50 -0500

    Refactor to hide API key, limit results in controller, and mock api call in tests

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -14,6 +14,7 @@
 require 'simplecov'
 SimpleCov.start 'rails'
 SimpleCov.add_filter ['spec', 'config']
+require 'webmock/rspec'
 #
 # See http://rubydoc.info/gems/rspec-core/RSpec/Core/Configuration
 RSpec.configure do |config|
